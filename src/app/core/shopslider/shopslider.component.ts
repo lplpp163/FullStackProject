5aa4b9739a2e0b86ee6fc4f866178e7cb61c63e7
Will <will889889@gmail.com>
2018-12-05 19:52:58 +0800

    修改Home的樣式

--- src/app/core/shopslider/shopslider.component.ts
+++ src/app/core/shopslider/shopslider.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopsliderComponent implements OnInit {
 
+  freshslide = 0;
+  maxSlide = 1;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  goRight() {
+    this.freshslide += 1;
+    if (this.freshslide > this.maxSlide) {
+      this.freshslide = 0;
+    }
+  }
+
+  goLeft() {
+    this.freshslide -= 1;
+    if (this.freshslide < 0) {
+      this.freshslide = this.maxSlide;
+    }
   }
 
 }
