9a72394d2318e4559062c3ea0de3060f53de36d0
ian287913 <45512766+ian287913@users.noreply.github.com>
2018-12-05 23:11:34 +0800

    FIX products-list page

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -12,7 +12,8 @@ import { HeaderComponent } from 'src/app/core/header/header.component';
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
-
+  pdts: Product[];
+  pdtLength = 0;
   searchKey: string;
   backend = `${environment.backend}`;
   filterCollapes = [
@@ -26,10 +27,30 @@ export class ProductsListComponent implements OnInit {
   constructor(private productsService: ProductsService,
     private router: Router,
     private activatedrouter: ActivatedRoute,
-    private cartService: CartService) { }
+    private cartService: CartService) {
+      localStorage.setItem('currentPage', '1');
+    }
 
   get products() {
+    this.pdts = this.productsService.products;
+    this.pdtLength = this.pdts.length;
     return this.productsService.products;
+  }
+
+  //  0~5
+  get userLevel() {
+    return (Number(localStorage.getItem('user_level')));
+  }
+  //  page helper funcs
+  get currentPage() {
+    return (Number(localStorage.getItem('currentPage')));
+  }
+  turnPage(num) {
+    localStorage.setItem('currentPage', (Number(localStorage.getItem('currentPage')) + num).toString() );
+    window.scroll(0, 0);
+  }
+  showPage(num) {
+    return (this.pdtLength >= ((this.currentPage + num) * 12));
   }
 
   ngOnInit() {
