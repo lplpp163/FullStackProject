fb52feb62b2ce2aad99ca038abeaa5975c31cd99
ian287913 <45512766+ian287913@users.noreply.github.com>
2018-12-06 22:35:55 +0800

    ProductPage

--- src/app/products/detail/detail.component.ts
+++ src/app/products/detail/detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductsService } from 'src/app/services/products.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
+  product: Product;
+  backend = `${environment.backend}`;
+  id = this.route.snapshot.paramMap.get('id');
 
-  constructor() { }
+
+  constructor(private cartService: CartService, private productsService: ProductsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.productsService.getProduct(this.id).subscribe((data: Product) => {
+      this.product = data;
+    });
+  }
+
+  add(id) {
+    this.cartService.add(id).subscribe((data: any) => {
+      if (data.success) {
+        location.reload();
+      }
+    });
   }
 
 }
