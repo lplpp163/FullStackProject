8c5c92d54b8afb94f73ebb5ea86d8a27e91e0934
Will <will889889@gmail.com>
2018-12-07 22:44:08 +0800

    首頁與後端連結(下面的製作中)

--- src/app/core/shopslider/shopslider.component.ts
+++ src/app/core/shopslider/shopslider.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductsService } from 'src/app/services/products.service';
+import { environment } from 'src/environments/environment';
+import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
   selector: 'app-shopslider',
@@ -7,11 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopsliderComponent implements OnInit {
 
+  backend = `${environment.backend}`;
   freshslide = 0;
   maxSlide = 1;
-  constructor() { }
+  listLen = 0;
+  pickindex: number[];
+
+  pickedOne: Product;
+  pickedTwo: Product;
+  pickedThree: Product;
+  pickedFour: Product;
+  constructor(private productsService: ProductsService) {
+  }
 
   ngOnInit() {
+    // this.listLen = this.productsService.products.length ? this.productsService.products.length : 0;
+    // console.log(this.productsService.products.length);
+    //  this.pickindex = [
+    //    Math.floor(Math.random() * this.listLen),
+    //    Math.floor(Math.random() * this.listLen),
+    //    Math.floor(Math.random() * this.listLen),
+    //    Math.floor(Math.random() * this.listLen),
+    // ];
   }
 
   goRight() {
